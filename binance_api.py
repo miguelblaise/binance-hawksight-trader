e1aa56754c2e441884b70534f91eecb9f76b8608
Blaise Ampil <blaiseampil@gmail.com>
2021-10-31 20:16:22 +0800

    added conditionals for buy and sell functions

--- binance_api.py
+++ binance_api.py
@@ -32,7 +32,12 @@ def buy_coins(pair):
     client = Client(API_KEY, SECRET_KEY)
     price = get_symbol_price(client, pair)
     transactions = client.get_my_trades(symbol=pair)
-    last_sell_transaction = [transaction for transaction in transactions if not transactions["isBuyer"]][-1]
+    last_transaction = [transaction for transaction in transactions][-1]
+
+    if not last_transaction["isBuyer"]:
+        print(f"{pair}: last transaction was sell. Skip buy.")
+        return
+    
     quote_qty = last_sell_transaction["quoteQty"]
 
     order_qty = round_down(client, coin, quote_qty/price)
@@ -47,9 +52,15 @@ def sell_coins(pair):
     client = Client(API_KEY, SECRET_KEY)
     price = get_symbol_price(client, pair)
     balance = client.get_asset_balance(asset=pair.replace("USDT", ""))
+
+    info = client.get_symbol_info(coin)
+    min_notional = [float(_['minNotional']) for _ in info['filters'] if _['filterType'] == 'MIN_NOTIONAL'][0]
     
 
     order_qty = round_down(client, coin, balance/price)
+    if order_qty <= min_notional:
+        print(f"{pair}: Total qty less than min sell qty")
+        return
 
     order = client.order_market_sell(
         symbol=coin,
@@ -60,14 +71,14 @@ def sell_coins(pair):
 if __name__ == "__main__":
     client = Client(API_KEY, SECRET_KEY)
     coin = "BUSDUSDT"
-    price = get_symbol_price(client, coin)
+    price = get_symbol_price(coin)
     # balance = float(client.get_asset_balance(asset='XRP')["free"])
     balance = 20
-    quantity = round_down(client, coin, balance/price)
+    quantity = round_down(coin, balance/price)
 
     response = client.get_symbol_info(coin)
 
-    print(quantity)
+    print(response)
     # order = client.order_market_buy(
     #     symbol=coin,
     #     quantity=quantity)
@@ -75,8 +86,8 @@ if __name__ == "__main__":
 
     # trades = client.get_all_orders(symbol=coin)
     # print(trades)
-    # trades = client.get_my_trades(symbol='XRPBUSD')
+    # trades = client.get_my_trades(symbol='XRPUSDT')
     # print(trades)
 
-    balance = client.get_asset_balance(asset='XRP')
-    print(balance)
+    # balance = client.get_asset_balance(asset='XRP')
+    # print(balance)
